f918a3b4b1a21100214850851579e1b4b9189527
Javier Moncada <moncadajavier2@yahoo.com>
2023-03-26 23:19:03 -0400

    Added CreateCharacterScreen class (Empty)

---
+++ src/main/java/net/mexicanminion/dndminecraft/gui/CreateCharacterScreen.java
@@ -0,0 +1,11 @@
+package net.mexicanminion.dndminecraft.gui;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class CreateCharacterScreen extends Screen {
+
+	protected CreateCharacterScreen(Text title) {
+		super(title);
+	}
+}
