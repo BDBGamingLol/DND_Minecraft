9563d6f3bfccd22d4cdf5b9715df8256f4e01554
Javier Moncada <moncadajavier2@yahoo.com>
2023-03-29 00:55:06 -0400

    Added Owo-lib (error when launch)

--- src/main/java/net/mexicanminion/dndminecraft/gui/CreateCharacterScreen.java
+++ src/main/java/net/mexicanminion/dndminecraft/gui/CreateCharacterScreen.java
@@ -1,11 +1,25 @@
 package net.mexicanminion.dndminecraft.gui;
 
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
+import io.wispforest.owo.ui.base.BaseOwoScreen;
+import io.wispforest.owo.ui.container.Containers;
+import io.wispforest.owo.ui.container.FlowLayout;
+import io.wispforest.owo.ui.core.OwoUIAdapter;
+import io.wispforest.owo.ui.core.Surface;
+import org.jetbrains.annotations.NotNull;
 
-public class CreateCharacterScreen extends Screen {
+public class CreateCharacterScreen extends BaseOwoScreen<FlowLayout> {
 
-	protected CreateCharacterScreen(Text title) {
-		super(title);
+
+	@Override
+	protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
+		return OwoUIAdapter.create(this, Containers::verticalFlow);
+	}
+
+	@Override
+	protected void build(FlowLayout rootComponent) {
+		rootComponent.surface(Surface.VANILLA_TRANSLUCENT);
+
 	}
 }
+
+
